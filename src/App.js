c991918ee5d48aed2bb5d56d5c2ae251aa83315f
Deepak Mokkapati <deepak.mokkapati@pwc.com>
2020-05-18 15:01:01 +0530

    dynamically show the date in the footer area

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 function App() {
+  const currentYear = new Date().getFullYear();
   return (
     <div className="grid-container">
       <header>
@@ -10,7 +11,7 @@ function App() {
         Product List
         </main>
       <footer>
-        Copyright  ©  All rights reserved
+        Copyright  © {currentYear}  All rights reserved
         </footer>
 
     </div>
